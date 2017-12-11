c2a5f6a9b64f2adf0c6bef8e676c2d83d702b2d1
Pierrickouw <pierrick@honestica.com>
2017-12-11 13:31:11 +0100

    add build number to version

--- create-build-info.js
+++ create-build-info.js
@@ -1,24 +1,23 @@
 #!/usr/bin/env node
 
-const git = require('git-rev');
-const fs = require('fs');
+const git = require("git-rev");
+const fs = require("fs");
 const originalPath = process.cwd();
 
-git.short(function (str) {
-
-  const versionFile = fs.readFileSync(`${originalPath}/pom.xml`, 'utf-8');
-  const regex = /<version>(.*)<\/version>/
-  const versionNumber = versionFile.match(regex);
+git.short(function(str) {
+  const versionFile = fs.readFileSync(`${originalPath}/pom.xml`, "utf-8");
+  const regex = /<version>(.*)<\/version>/;
+  const versionNumber = process.env.VERSION ? process.env.VERSION : "dev";
 
   const build = {
     lastcommit: str,
-    version: versionNumber[1] ? versionNumber[1] : 'notspecifed'
+    version: versionNumber ? versionNumber : "notspecifed"
   };
 
-  fs.writeFile(`${originalPath}/build.json`, JSON.stringify(build), (err) => {
+  fs.writeFile(`${originalPath}/build.json`, JSON.stringify(build), err => {
     if (err) {
       throw err;
     }
-    console.log('Wrote build.json');
+    console.log("Wrote build.json");
   });
-})
+});
